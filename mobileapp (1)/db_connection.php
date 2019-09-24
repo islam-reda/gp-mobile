4cd803145c8d85030ecf04373be9db71616609a6
islam-reda <38462856+islam-reda@users.noreply.github.com>
2019-09-24 17:07:35 +0200

    Update db_connection.php

--- mobileapp (1)/db_connection.php
+++ mobileapp (1)/db_connection.php
@@ -9,8 +9,8 @@ class connection {
   public $conn;
   function __construct(){
     $this->dsn = 'mysql:host=192.168.1.107:3306;dbname=crm_db';
-    $this->user = 'djvuser';
-    $this->password = 'dejavu';
+    $this->user = 'user';
+    $this->password = 'password';
     $this->options=array(
         PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8 '
     );
